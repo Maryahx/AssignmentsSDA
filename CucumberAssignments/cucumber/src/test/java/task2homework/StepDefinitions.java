35c995373ea52407713e6f15fcbc8a42bb159185
Maryah-sa <maryahalshimer1@gmail.com>
2024-03-29 13:58:36 +0300

    Task2 HomeworkScenarioOutline

--- CucumberAssignments/cucumber/src/test/java/task2homework/StepDefinitions.java
+++ CucumberAssignments/cucumber/src/test/java/task2homework/StepDefinitions.java
@@ -7,11 +7,6 @@ import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
-
-
 
 public class StepDefinitions {
     WebDriver driver;
