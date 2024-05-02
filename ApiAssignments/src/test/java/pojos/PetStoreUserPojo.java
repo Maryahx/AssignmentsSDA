8c8f27a8d86bb18a2e5ede08b2e2239bdae8ce6d
Maryah-sa <maryahalshimer1@gmail.com>
2024-05-02 08:57:30 +0300

    Homework 13 Pojo

---
+++ ApiAssignments/src/test/java/pojos/PetStoreUserPojo.java
@@ -0,0 +1,105 @@
+package pojos;
+
+public class PetStoreUserPojo {
+    private Integer id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private Integer userStatus;
+
+    public PetStoreUserPojo() {
+    }
+
+    public PetStoreUserPojo(Integer id, String username, String firstName, String lastName, String email, String password, String phone, Integer userStatus) {
+        this.id = id;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.userStatus = userStatus;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUserStatus(Integer userStatus) {
+        this.userStatus = userStatus;
+    }
+
+    public Integer getUserStatus() {
+        return userStatus;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "UserPojo{" +
+                        "firstName = '" + firstName + '\'' +
+                        ",lastName = '" + lastName + '\'' +
+                        ",password = '" + password + '\'' +
+                        ",userStatus = '" + userStatus + '\'' +
+                        ",phone = '" + phone + '\'' +
+                        ",id = '" + id + '\'' +
+                        ",email = '" + email + '\'' +
+                        ",username = '" + username + '\'' +
+                        "}";
+    }
+}
